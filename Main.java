a043fcbf02985a11b573369738b139942ef77861
bazzl1202 <bazzl1202@wrdsb.ca>
2020-09-15 16:42:02 +0000

    Now puts numbers in ascending order.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Puts numbers in ascending order
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask the user to enter the first integer
+    System.out.println("Please enter an integer:");
+
+    //declare and initialize variable to store integer one
+    int integerOne = input.nextInt();
+
+    //ask the user to enter the second integer
+    System.out.println("Please enter another integer:");
+
+    //declare and initialize variable to store integer two
+    int integerTwo = input.nextInt();
+
+    // checks to see what numbers are large
+    if( integerOne > integerTwo ){
+      System.out.println("Your numbers in ascending order are " + integerTwo + ", " + integerOne);
+    } else if( integerOne < integerTwo){
+    System.out.println("Your numbers in ascending order are " + integerOne + ", " + integerTwo);
+    } else {
+      System.out.println("Your numbers in ascending order are " + integerOne + ", " + integerTwo);
+    }
+    
     
   }
 }
